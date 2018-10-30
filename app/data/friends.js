ebd8f2a2b604dfb8d608749a28dc4396d3e39177
Binh Lam <cindybinh444@gmail.com>
2018-10-29 23:36:26 -0400

    display friends data

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friendsArray =
+[{
+        "name": "Ahmed",
+        "photo": "https://archive.li/ChwA6/907f642ed975dd1afc9c7f84ded23652c44b0f89.gif",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Mandy",
+        "photo": "http://www.clker.com/cliparts/O/X/h/8/B/j/girl-cartoon-md.png",
+        "scores": [
+            3,
+            1,
+            2,
+            4,
+            5,
+            5,
+            5,
+            5,
+            5,
+            2
+        ]
+
+    }
+
+];
+
+module.exports = friendsArray;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,16 +9,65 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+// ------------------apiRoutes------------------
+
+    //Load Data (require Data) from friends.js file
+    var friendsData = require("../data/friends");
+
+    //API GET request to parse those Data into JSON
+    //Getting information
+    app.get("/api/friends", function(req,res){
+        res.json(friendsData);
+    });
+
+
+    //API Post request to submit into Data 
+    app.post("/api/friends", function(req,res) {
+        //making more user-friendly function by adding if-else statement
+        //if (){ 
+        friendsData.push(req.body);
+        //figure out which friend or what is the best match??? 
+        //store in a variable of best_match object
+        //best_match: best_friend 
+        //doing the whole comparison logic in here 
+        res.json({ok: true, friends: friendsData});
+        //using if-else statement (res.json({ok: false}))
+        //}else {
+        //req.body.name="";
+        //res.json({ok:false});
+    //}
+    });
+
+    //API Data Clear out!
+    app.post("/api/clear", function(req, res) {
+        // Empty out the arrays of data
+        friendsData.length = [];
+    
+        res.json({ ok: true });
+      });
+
+
+// ------------------apiRoutes------------------
+
+// ---------------- htmlRoutes-------------
+
 //Linking external css to HTML
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get("/survey.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
-app.get("/survey.html", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
+//------------------- htmlRoutes---------------
+
+//any routes
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
