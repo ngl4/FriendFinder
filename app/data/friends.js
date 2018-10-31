d54e97afa96bc2b731a76bc487a60726809f4012
Binh Lam <cindybinh444@gmail.com>
2018-10-31 11:23:59 -0400

    edit data and comment out savebtn

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,4 @@
-var friendsArray =
-[{
+var friendsArray = [{
         "name": "Ahmed",
         "photo": "https://archive.li/ChwA6/907f642ed975dd1afc9c7f84ded23652c44b0f89.gif",
         "scores": [
@@ -31,6 +30,38 @@ var friendsArray =
             2
         ]
 
+    },
+    {
+        "name": "Kiki",
+        "photo": "https://image1.masterfile.com/getImage/632-02344446em-young-japanese-woman-reading-mangastyle-comic-book-stock-photo.jpg",
+        "scores": [
+            2,
+            3,
+            3,
+            2,
+            3,
+            2,
+            1,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Lim",
+        "photo": "https://image1.masterfile.com/getImage/632-02344445em-young-japanese-man-reading-comic-book-in-store-stock-photo.jpg",
+        "scores": [
+            2,
+            4,
+            4,
+            3,
+            3,
+            1,
+            3,
+            5,
+            3,
+            3
+        ]
     }
 
 ];
